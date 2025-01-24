d3854b0f650949a0e8650aa2fcae792fda53aa91
eckstein3rdfret <95610768+eckstein3rdfret@users.noreply.github.com>
2025-01-23 21:27:16 -0800

    Add files via upload
    Testing 456

---
+++ doubleInt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int input_number;
+    cout << "Please input a number and we will double it!:";
+    cin >> input_number;
+    cout << "Your doubled number is:" << input_number * 2;
+    return 0;
+}
+
+//
+// Created by eckst on 1/23/2025.
+//
