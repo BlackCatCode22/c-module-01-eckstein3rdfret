718a841feadb1d19533a518b887d247295639cad
eckstein3rdfret <95610768+eckstein3rdfret@users.noreply.github.com>
2025-01-25 16:58:47 -0800

    Summing of Two Ints

---
+++ sumTwoInts.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x;
+    int y;
+
+    cout << "Welcome to Sum Two Ints" << endl;
+    cout << "Please enter your first Int!" << endl;
+    cin >> x;
+    cout << "Great! Now enter your second Int!" << endl;
+    cin >> y;
+    cout << "AND NOW FOR YOUR SUM OF TWO INTS\n";
+    cout << "Sum is! ------>" << x + y << endl;
+    cout << "Thanks for playing!";
+    return 0;
+}
