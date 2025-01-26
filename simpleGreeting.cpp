cd501aa8883701f71c6c020c4d86c721e4df8742
eckstein3rdfret <95610768+eckstein3rdfret@users.noreply.github.com>
2025-01-25 17:10:10 -0800

    Hello There!

---
+++ simpleGreeting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string username;
+    cout << "(Uses best Obi-wan impression....)\nHello There!" << endl;
+    cout << "The Galaxy is a big place, I'm not surprised we haven't met\n";
+    cout << "What should I call you friend?\n";
+    cin >> username;
+    cout << "Hmm.." << username <<"...\n";
+    cout << "That is an odd name\n";
+    cout << "Well.." << username << "! \n" << "It's great to meet a new friend!\n";
+    cout << "May the Force be with you!\n";
+
+    return 0;
+}
